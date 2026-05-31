e06746cd9a934a3052bc7baa8489add28ea77695
Bashar Almuntaser <basharalmuntaser2@gmail.com>
2026-05-31 12:38:21 +0300

    Delete backend/fix_icons.sql

--- backend/fix_icons.sql
+++
@@ -1,6 +0,0 @@
-UPDATE categories SET icon = '☕' WHERE id = 1;
-UPDATE categories SET icon = '🥤' WHERE id = 2;
-UPDATE categories SET icon = '🥐' WHERE id = 3;
-UPDATE categories SET icon = '🍳' WHERE id = 4;
-UPDATE categories SET icon = '🍽️' WHERE id = 5;
-UPDATE categories SET icon = '🍰' WHERE id = 6;
